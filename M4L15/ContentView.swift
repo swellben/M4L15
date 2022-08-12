b4cee2a62e86a3fd382be0784893d16e2251225b
Ben Moodie <swellben@gmail.com>
2022-08-12 07:01:22 -0600

    Initial Commit

---
+++ M4L15/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  M4L15
+//
+//  Created by Ben Moodie on 2022-08-12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ M4L15/M4L15App.swift
@@ -0,0 +1,17 @@
+//
+//  M4L15App.swift
+//  M4L15
+//
+//  Created by Ben Moodie on 2022-08-12.
+//
+
+import SwiftUI
+
+@main
+struct M4L15App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
